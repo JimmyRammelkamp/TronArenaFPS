c9db9d991c3b6c8ecf3400a64babd5510f7a4416
Jimmy <k1821317@kingston.ac.uk>
2022-10-27 17:18:29 +0100

    kyle the robot, server rpc spawning

--- Hello World/Assets/PlayerController.cs
+++ Hello World/Assets/PlayerController.cs
@@ -13,14 +13,15 @@ public class PlayerController : NetworkBehaviour
     [SerializeField]
     private Transform fpcam;
     private Camera topcam;
+    [SerializeField] private Transform prefab;
 
     [SerializeField]
     TextMesh nick;
 
-    NetworkVariable<float> forward = new NetworkVariable<float>(0,NetworkVariableReadPermission.Everyone,NetworkVariableWritePermission.Owner);
-    NetworkVariable<float> turn = new NetworkVariable<float>(0,NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Owner);
+    //NetworkVariable<float> forward = new NetworkVariable<float>(0,NetworkVariableReadPermission.Everyone,NetworkVariableWritePermission.Owner);
+    //NetworkVariable<float> turn = new NetworkVariable<float>(0,NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Owner);
 
-    NetworkVariable<FixedString128Bytes> nickname = new NetworkVariable<FixedString128Bytes>("", NetworkVariableReadPermission.Everyone,NetworkVariableWritePermission.Owner);
+    //NetworkVariable<FixedString128Bytes> nickname = new NetworkVariable<FixedString128Bytes>("", NetworkVariableReadPermission.Everyone,NetworkVariableWritePermission.Owner);
 
     // Start is called before the first frame update
     void Start()
@@ -35,34 +36,51 @@ public class PlayerController : NetworkBehaviour
     {
         if (IsOwner)
         {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                SpawnServerRpc();
+            }
             float forward = Input.GetAxis("Vertical");
             float turn = Input.GetAxis("Horizontal") + Input.GetAxis("Mouse X");
-           // RelayInputServerRpc(forward, turn);
-           
+            transform.Translate(new Vector3(0, 0, forward * walkSpeed * Time.deltaTime));
+            transform.Rotate(new Vector3(0, turn * turnSpeed * Time.deltaTime, 0));
+            //RelayInputServerRpc(forward *walkSpeed * Time.deltaTime, turn * turnSpeed * Time.deltaTime); //this needs time delta time
+
         }
-        transform.Translate(new Vector3(0, 0, forward.Value * walkSpeed * Time.deltaTime));
-        transform.Rotate(new Vector3(0, turn.Value * turnSpeed * Time.deltaTime, 0));
+        //if(IsServer)
+        //{
+        //    transform.Translate(new Vector3(0, 0, forward.Value * walkSpeed * Time.deltaTime)); //this is for network variable isntead of rpc
+        //    transform.Rotate(new Vector3(0, turn.Value * turnSpeed * Time.deltaTime, 0));
+        //}
 
 
         float tilt = Input.GetAxis("Mouse Y");
         if (fpcam != null)
             fpcam.Rotate(new Vector3(-tilt * tiltSpeed * Time.deltaTime, 0));
-        if (Input.GetKeyDown("Space"))
+        if (Input.GetKeyDown(KeyCode.Space))
         {
             //swap cameras
             topcam.enabled = !topcam.enabled;
             fpcam.GetComponent<Camera>().enabled = !fpcam.GetComponent<Camera>().enabled;
         }
 
-        nick.text = nickname.Value.ToString();
+        //nick.text = nickname.Value.ToString();
     }
 
-    //[ServerRpc]
-    //void RelayInputServerRpc(float forward, float turn)
-    //{
-    //    transform.Translate(new Vector3(0, 0, forward * walkSpeed * Time.deltaTime));
-    //    transform.Rotate(new Vector3(0, turn * turnSpeed * Time.deltaTime, 0));
-    //}
+    [ServerRpc]
+    void RelayInputServerRpc(float forward, float turn)
+    {
+        transform.Translate(new Vector3(0, 0, forward));
+        transform.Rotate(new Vector3(0, turn, 0));
+    }
+
+    [ServerRpc]
+    void SpawnServerRpc()
+    {
+        var obj = Instantiate(prefab);
+        obj.position = transform.position + 4 * transform.forward;
+        obj.GetComponent<NetworkObject>().Spawn(true);
+    }
 
     public override void OnNetworkSpawn()
     {
@@ -93,8 +111,8 @@ public class PlayerController : NetworkBehaviour
         }
     }
 
-    public void SetNickname(string name)
-    {
-        nickname.Value = name;
-    }
+    //public void SetNickname(string name)
+    //{
+    //    nickname.Value = name;
+    //}
 }
