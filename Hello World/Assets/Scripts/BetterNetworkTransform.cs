a8097281b278b8451af74642b96ce5fc7791eaea
JimmyRammelkamp <jimi334455@gmail.com>
2022-11-26 16:02:58 +0000

    Wipe

--- Hello World/Assets/Scripts/BetterNetworkTransform.cs
+++
@@ -1,9 +0,0 @@
-using Unity.Netcode.Components;
-
-public class BetterNetworkTransform : NetworkTransform
-{
-    protected override bool OnIsServerAuthoritative()
-    {
-        return false;
-    }
-}
