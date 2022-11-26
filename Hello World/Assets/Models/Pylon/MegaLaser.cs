8da15e24fbdc045f29f01129b1d116f8e49b8897
JimmyRammelkamp <jimi334455@gmail.com>
2022-11-26 21:00:51 +0000

    Rising Wall sync fix, Mega Laser Damage, Deathmatch Manager Canvas Adjustment

--- Hello World/Assets/Models/Pylon/MegaLaser.cs
+++ Hello World/Assets/Models/Pylon/MegaLaser.cs
@@ -1,14 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.Netcode;
 
-public class MegaLaser : MonoBehaviour
+public class MegaLaser : NetworkBehaviour
 {
     LineRenderer LR;
+
+    HelloWorldManager worldManager;
+
     // Start is called before the first frame update
     void Start()
     {
         LR = GetComponent<LineRenderer>();
+
+        worldManager = FindObjectOfType<HelloWorldManager>();
+
     }
 
     // Update is called once per frame
@@ -17,4 +24,10 @@ public class MegaLaser : MonoBehaviour
         LR.SetPosition(0, transform.position);
         LR.SetPosition(1, transform.position + transform.up * 100);
     }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        worldManager.DamagePlayerClientRpc(other.GetComponent<PlayerController>(), 1000);
+    }
+
 }
